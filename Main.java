a1c7d9b8057d82a1d70b4722ae1c7a7008c593f9
Eric Jefferson <ejefferson50@gmail.com>
2019-02-06 15:03:15 -0500

    Finished Input Sum

--- Main.java
+++ Main.java
@@ -1,9 +1,35 @@
+import java.util.Scanner;
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
+    
 
     public static void main(String[] args){
+        int a, b, c;
+        
+     
+        Scanner add = new Scanner(System.in);
+        System.out.println("Let's Do Some Addition! Enter Your Number");
+        a = add.nextInt();
+        b = 1;
+        c = a + b;
+        
+        System.out.println("Your Answer Equals " + c);
+        
+        
+        
+        
+        
+        
+        
+      
+        
+        
+        
+        
+        
+        
 
     }
 }
